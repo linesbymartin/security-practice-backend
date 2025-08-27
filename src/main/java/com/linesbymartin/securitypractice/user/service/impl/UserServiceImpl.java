7bba08bb54391a3a5cedef9a8a2bda0aa607c40c
linesbymartin <contact@linesbymartin.com>
2025-08-27 15:11:52 +0300

    feat: updated create method of UserServiceImpl.java

--- src/main/java/com/linesbymartin/securitypractice/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/linesbymartin/securitypractice/user/service/impl/UserServiceImpl.java
@@ -1,16 +1,17 @@
 package com.linesbymartin.securitypractice.user.service.impl;
 
 import com.linesbymartin.securitypractice.common.service.impl.CrudServiceImpl;
+import com.linesbymartin.securitypractice.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.linesbymartin.securitypractice.user.service.UserService;
+import com.linesbymartin.securitypractice.user.dto.UserResponseDto;
 import com.linesbymartin.securitypractice.user.domain.UserEntity;
 import com.linesbymartin.securitypractice.user.dto.UserCreateDto;
-import com.linesbymartin.securitypractice.user.dto.UserResponseDto;
 import com.linesbymartin.securitypractice.user.dto.UserUpdateDto;
 import com.linesbymartin.securitypractice.user.mapper.UserMapper;
-import com.linesbymartin.securitypractice.user.repository.UserRepository;
-import com.linesbymartin.securitypractice.user.service.UserService;
+import org.springframework.transaction.annotation.Transactional;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -20,20 +21,26 @@ public class UserServiceImpl extends CrudServiceImpl<UserEntity, UUID, UserRespo
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         super(userRepository, userMapper);
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
+    @Transactional
     public UserResponseDto create(UserCreateDto dto) {
         if (userRepository.existsByEmail(dto.getEmail())){
             throw new EntityExistsException("User with email " + dto.getEmail() + " already exists");
         }
 
-        return super.create(dto);
+        UserEntity entity = userMapper.fromCreate(dto);
+        entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+        UserEntity saved = userRepository.save(entity);
+        return userMapper.toResponse(saved);
     }
 
 }
