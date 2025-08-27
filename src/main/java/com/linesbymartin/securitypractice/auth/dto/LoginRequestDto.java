56885f9f4cafc735e06ffa64c69b83ff0e96232b
linesbymartin <contact@linesbymartin.com>
2025-08-27 15:00:43 +0300

    feat: created LoginRequestDto.java

---
+++ src/main/java/com/linesbymartin/securitypractice/auth/dto/LoginRequestDto.java
@@ -0,0 +1,19 @@
+package com.linesbymartin.securitypractice.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDto {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
