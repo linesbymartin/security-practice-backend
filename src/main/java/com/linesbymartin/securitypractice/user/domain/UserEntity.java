e0b7b9d801c20de9a11990442cf04a09687f11b3
linesbymartin <contact@linesbymartin.com>
2025-08-27 15:02:42 +0300

    feat: implemented UserDetails and added role property to UserEntity.java

--- src/main/java/com/linesbymartin/securitypractice/user/domain/UserEntity.java
+++ src/main/java/com/linesbymartin/securitypractice/user/domain/UserEntity.java
@@ -1,18 +1,22 @@
 package com.linesbymartin.securitypractice.user.domain;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import com.linesbymartin.securitypractice.common.domain.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class UserEntity extends BaseEntity {
+public class UserEntity extends BaseEntity implements UserDetails {
 
     @Column(nullable = false, length = 254, unique = true) // the max length of an email is 254
     private String email;
@@ -25,4 +29,18 @@ public class UserEntity extends BaseEntity {
 
     @Column(nullable = false, length = 100)
     private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(getRole().name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
 }
